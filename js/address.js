ccd3026a11ce81973a702f6c7faa2b054b2976c4
babybei <1061195835@qq.com>
2017-09-15 17:24:47 +0800

    地址选择

--- js/address.js
+++ js/address.js
@@ -4,7 +4,11 @@
 new Vue({
     el:".page",
     data:{
-        addressList:[]
+        limitNum:3,
+        addressList:[],
+        currentIndex:0,
+        hoverIndex:-1,
+        shipingMethod:1
     },
     mounted:function(){
         this.$nextTick(function(){
@@ -13,7 +17,7 @@ new Vue({
     },
     computed: {
         filterAddress: function () {
-            return this.addressList.slice(0, 3)
+            return this.addressList.slice(0, this.limitNum)
         }
     },
     methods:{
@@ -23,8 +27,32 @@ new Vue({
                 var res=response.data;
                 if(res.status=="0"){
                     _this.addressList=res.result;
+                    _this.currentIndex=res.defaultIndex;
                 }
             });
+        },
+        setDefault:function(addressId){
+            this.addressList.forEach(function(address,index){
+               if(address.addressId==addressId){
+                   address.isDefault=true;
+               }else{
+                   address.isDefault=false;
+               }
+            });
         }
+        //addressHover:function(e,active){  //鼠标滑过
+        //    var dom =e.target;  //当前元素
+        //    var classVal=dom.getAttribute("class");
+        //    if(active) {
+        //        //添加class
+        //        classVal = classVal.concat(" addr-active");
+        //        dom.setAttribute("class", classVal);
+        //    }
+        //    else{
+        //        //删除calss
+        //        classVal = classVal.replace(" addr-active","");
+        //        dom.setAttribute("class", classVal);
+        //    }
+        //}
     }
 });
